c939c89d789320c26632b8c05a909c8997ca234d
MEANBEANMACHINE\Kristina <kristina.benevides@live.se>
2018-11-06 17:07:42 +0100

    Update funcA.py

--- funcA.py
+++ funcA.py
@@ -1,36 +1,38 @@
 import gzip
+import os
+
+files = []
+for name in os.listdir('sample_data'):
+	files.append(gzip.open('sample_data/' + name))
+
+#path = 'C:/Users/Kristina/Documents/GitHub/great_apes_project/sample_data'
 
 file = 'sample_data/Gorilla_beringei_graueri-Serufuli.mpile.gz'
-tup = []
-groups = {}
-lens = []
-with gzip.open(file) as f:
-	for line in f:
-		split = line.strip().split("\t")
-		tup.append((split[0], split[3]))
-#print tup
-for x,y in tup:
-    group = groups.get(x, [])
-    group.append(y)
-    groups[x] = group
-for x in groups:
-	print(x,groups[x])
-	num_cov= len(groups[x])
-	print(num_cov)
-#for x in groups[x]:
-#	lens.append(len(x))
-#    print(lens)
-#num_nuc = sum(lens)
-#print(num_nuc)
 
-	#abundance = float(num_nuc / num_cov)
-	#print(num_cov,num_nuc)
-    #print(abundance)
+def function_A(file):
+	#Function which estimate the abundance (number of copies) of each virus for an individual.
 
+	tup = []
+	groups = {}
+	output = {}
 
-#print(len(groups['AC_000007.1']))
-#print(groups['AC_000007.1'].count("A") + groups['AC_000007.1'].count("G") + groups['AC_000007.1'].count("C") + groups['AC_000007.1'].count("T"))
+	#Open file
+	with gzip.open(file) as f:
+		for line in f:
+			split = line.strip().split("\t")
+			tup.append((split[0], split[3]))
 
+	for x,y in tup:
+		group = groups.get(x, [])
+		group.append(y)
+		groups[x] = group
 
+	for x in groups:
+		num_cov= len(groups[x])
+		lens  = [len(s) for s in groups[x]]
+		num_nuc = sum(lens)
+		abundance = float(num_nuc/num_cov)
+		output[x] = abundance
+	print output
 
-
+print function_A(file)
