61b06f97a1ff6eb075e841508c37af77bf4e9b8f
Emma-laptop <kottdjur@hotmail.com>
2018-11-06 11:58:03 +0100

    Update function_B.py

--- function_B.py
+++ function_B.py
@@ -1,4 +1,6 @@
 import gzip
+from collections import defaultdict
+
 """
 NC_0014222 is phiX = not a true signal but a virus added later to the s
 sample
@@ -7,14 +9,24 @@ sample
 sample_file = "sample_data/Gorilla_beringei_beringei-Imfura.mpile.gz"
 virus_size_file = "sample_data/virus_genome_sizes.tsv"
 
+# Read virus sizes into dict
 virus_sizes = {}
 with open(virus_size_file) as f1:
     for line in f1:
-        print line
         (key, value) = line.strip().split(" ")
-        virus_sizes[key] = value
+        virus_sizes[key] = int(value)
+f1.close()
 
-#with gzip.open(filename) as f2:
-#    for line in f2:
-#        splitted = line.strip().split("\t")
-#        print splitted
+# Read mapped nucleotides into a dict of lists containing the number of maps per nuc
+# Example result: 'NC_015050.1': [1, 1, 2, 3, 4, 5, 5, 2, 4, 5, 5, 5, 3]
+maps = defaultdict(list)
+with gzip.open(sample_file) as f2:
+    for line in f2:
+        (seqid, loc, nuc, nucmap) = line.strip().split("\t")
+        maps[seqid].append(len(nucmap))
+
+# Calculate nr of unmapped nucs for each virus
+unmapped = {}
+for key in maps:
+    unmapped[key] = virus_sizes[key] - len(maps[key])
+
