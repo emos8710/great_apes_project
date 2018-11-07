d51bc650bdd215ba3f204d8817b6c708aad5de35
MEANBEANMACHINE\Kristina <kristina.benevides@live.se>
2018-11-07 13:59:06 +0100

    Update fetch_names.py

--- fetch_names.py
+++ fetch_names.py
@@ -1,10 +1,29 @@
 from Bio import Entrez
+import csv
 
-file = open("sample_data/virus_genome_sizes.tsv")
+# ============================
 
-virus_id = []
-for line in file:
-    split = line.strip().split(" ")
-    virus_id.append(split[0])
+# Define input and output file
+virus_filename = 'sample_data/virus_genome_sizes.tsv'
+virus_output = 'sample_data/virus_names.tsv'
 
+virus_ids = []
 
+# Save all 9569 virus id:s
+with open(virus_filename) as f:
+    for line in f:
+        split = line.strip().split(" ")
+        virus_ids.append(split[0])
+
+# Fetch fasta files for each virus id from NCBI:s nuccore database
+Entrez.email = 'kristina.benevides@live.se'
+handle = Entrez.efetch(db='nuccore', id=virus_ids, rettype="fasta", retmode="xml")
+response = Entrez.read(handle, validate = False)
+
+# Save accession number and organism name in a tsv file
+with open(virus_output,'wt') as outfile:
+    for entry in response:
+        tsv_writer = csv.writer(outfile, delimiter='\t')
+        tsv_writer.writerow((entry.get('TSeq_accver'),entry.get('TSeq_orgname')))
+
+# ============================
