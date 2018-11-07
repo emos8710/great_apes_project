d1f6d9042a1f50722939b2ff261ec74c2c4fba95
HildaAndersson <hilda.asaandersson@gmail.com>
2018-11-07 11:16:52 +0100

    new functions and new main to use all the functions

---
+++ functionA.py
@@ -0,0 +1,30 @@
+def function_A(file):
+	# Function which estimate the abundance (number of copies) of each virus for an individual and stores this information
+	# in a dictionary.
+
+	tup = []
+	groups = {}
+	output = {}
+
+	# Assumes file format with rows containing NCBI virus id, position on the virus genome, virus genome nucelotide,
+	# nucleotides found in the great ape sample, for example ['NC_001993.1', '229479', 'T', 'TTTT'].
+	with gzip.open(file) as f:
+		for line in f:
+			split = line.strip().split("\t")
+			tup.append((split[0], split[3]))
+			
+    # Dictionary with NCBI virus id:s as keys and abundance of virus (number of virus copies present) as values.
+	# For example: {'NC_008892.1': 6.2272727272727275, 'NC_006276.1': 2.65, 'NC_016447.1': 1.5583756345177664, ...}
+	for x,y in tup:
+		group = groups.get(x, [])
+		group.append(y)
+		groups[x] = group
+
+	for x in groups:
+		num_cov= len(groups[x])
+		lens  = [len(s) for s in groups[x]]
+		num_nuc = sum(lens)
+		abundance = float(num_nuc)/float(num_cov)
+		output[x] = abundance
+	return output
+
---
+++ functionB.py
@@ -0,0 +1,42 @@
+def functionB(sample_file):
+
+	#Output: Dict with virus codes as key, value = [%unmapped, mean, std, good]
+
+	virus_size_file = "sample_data/virus_genome_sizes.tsv"
+
+	# Read virus sizes into dict
+	virus_sizes = {}
+	with open(virus_size_file) as f1:
+	    for line in f1:
+		(key, value) = line.strip().split(" ")
+		virus_sizes[key] = int(value)
+	f1.close()
+
+	# Read mapped nucleotides into a dict of lists containing the number of maps per nuc
+	# Example result: 'NC_015050.1': [1, 1, 2, 3, 4, 5, 5, 2, 4, 5, 5, 5, 3]
+	maps = defaultdict(list)
+	with gzip.open(sample_file) as f2:
+	    for line in f2:
+		(seqid, loc, nuc, nucmap) = line.strip().split("\t")
+		maps[seqid].append(len(nucmap))
+	f2.close()
+
+	# Calculate % of unmapped nucs for each virus
+	unmapped = {}
+	for key in maps:
+	    unmapped[key] = float((virus_sizes[key] - len(maps[key]))) / virus_sizes[key]
+
+	# Calculate mean, standard deviation and expected mean of data.
+	# Data with mean within one std of the expected mean are good.
+	stats = {}
+	for key in maps:
+	    temp_mean = np.mean(maps[key])
+	    temp_std = np.std(maps[key])
+	    exp_mean = (min(maps[key]) + max(maps[key])) / 2
+	    if (temp_mean <= exp_mean + temp_std) and (temp_mean >= exp_mean - temp_std):
+		good = 1
+	    else:
+		good = 0
+	    stats[key] = [unmapped[key], temp_mean, temp_std, good]
+
+	return stats
