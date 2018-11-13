cba7ed052efa97b24187cbb6b05db2e6dbbb5a9f
Emma-laptop <kottdjur@hotmail.com>
2018-11-13 15:06:47 +0100

    Standalone map percent function

--- Old functions/Version2/Function_B/map_percent_filter.py
+++ Old functions/Version2/Function_B/map_percent_filter.py
@@ -1,4 +1,7 @@
-def map_percent_filter(maps, threshold, virus_sizes):
+def map_percent_filter(sample_file, threshold, virus_sizes):
+    from collections import defaultdict
+    import gzip
+
     """
     Takes a dict with number of mapped nucleotides per virus nucleotide, a threshold (0 - 1) and  dict with virus sizes.
     Outputs a dict with the viruses that pass the threshold and their mapped %.
@@ -7,11 +10,18 @@ def map_percent_filter(maps, threshold, virus_sizes):
     Output item example: 'NC_015050.1': 0.99
     """
 
+    mapped = defaultdict(int)
+    with gzip.open(sample_file) as f1:
+        for line in f1:
+            (seqid, loc, nuc, nucmap) = line.strip().split("\t")
+            mapped[seqid] = mapped[seqid] + 1
+    f1.close()
+
     # Calculate % of mapped nucs for each virus and check with threshold
     mapped_over_th = {}
-    for key in maps:
-        mapped = float(len(maps[key])) / virus_sizes[key]
-        if mapped >= threshold:
-            mapped_over_th[key] = mapped
+    for key in mapped:
+        map_perc = float(mapped[key]) / virus_sizes[key]
+        if map_perc >= threshold:
+            mapped_over_th[key] = map_perc
 
     return mapped_over_th
